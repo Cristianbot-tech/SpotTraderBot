70e6eda1dd7832144621de8e65d8508ec7544fba
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-16 18:19:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ if st.button("Iniciar Bot"):
             closes.append(float(candle["close"]))
         precio_actual = closes[-1]
         df = pd.DataFrame(closes, columns=["close"])
+        st.line_chart(df["close"])
         ema9 = df["close"].ewm(span=9).mean().iloc[-1]
         ema21 = df["close"].ewm(span=21).mean().iloc[-1]
 
