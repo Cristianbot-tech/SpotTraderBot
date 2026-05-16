ace10e3fdb80d6a0dd8d730dcbb9450244166fcc
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-16 18:53:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,10 @@ if st.button("Iniciar Bot"):
 
         ema9 = df["EMA9"].iloc[-1]
         ema21 = df["EMA21"].iloc[-1]
-
         grafico.line_chart(
-            df[["close", "EMA9", "EMA21"]],
+            df[["EMA9", "EMA21"]],
             height=400
         )
-
         with panel.container():
 
             st.write("Precio actual:", precio_actual)
