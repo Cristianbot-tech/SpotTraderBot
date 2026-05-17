7130f378a3dd9e47726c7a2286ade53deff57065
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-17 06:18:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,9 +138,9 @@ if st.session_state.bot_activo:
                height=500,
                xaxis_rangeslider_visible=False
             )
-           
-            st.plotly_chart(fig, use_container_width=True)
-        with panel.container():
+           with grafico.container():
+               st.plotly_chart(fig, use_container_width=True)
+           with panel.container():
 
                 cambio = precio_actual - closes[-2]
 
