dbbcbb809559f4050747b5233e32fd026b6af90d
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-18 04:26:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,126 +51,126 @@ if st.session_state.bot_activo:
         f"https://api.coinex.com/v2/spot/kline"
         f"?market={market}&period={timeframe}&limit=50"
     )
-            response = requests.get(url)
-            data = response.json()
+    response = requests.get(url)
+    data = response.json()
 
-            opens = []
-            highs = []
-            lows = []
-            closes = []
-            volumes = []
+    opens = []
+    highs = []
+    lows = []
+    closes = []
+    volumes = []
 
-            for candle in data["data"]:
-                opens.append(float(candle["open"]))
-                highs.append(float(candle["high"]))
-                lows.append(float(candle["low"]))
-                closes.append(float(candle["close"]))
-                volumes.append(float(candle["volume"]))
-            precio_actual = closes[-1]
-            df = pd.DataFrame({
-                "open": opens[-100:],
-                "high": highs[-100:],
-                "low": lows[-100:],
-                "close": closes[-100:],
-                "volume": volumes[-100:]
-            })
-            df["EMA9"] = df["close"].ewm(span=9).mean()
-            df["EMA21"] = df["close"].ewm(span=21).mean() 
-            volumen_box.metric(
-                "📊 Volumen",
-                 round(df["volume"].iloc[-1], 2)
-            )
+    for candle in data["data"]:
+        opens.append(float(candle["open"]))
+        highs.append(float(candle["high"]))
+        lows.append(float(candle["low"]))
+        closes.append(float(candle["close"]))
+        volumes.append(float(candle["volume"]))
+    precio_actual = closes[-1]
+    df = pd.DataFrame({
+        "open": opens[-100:],
+        "high": highs[-100:],
+        "low": lows[-100:],
+        "close": closes[-100:],
+        "volume": volumes[-100:]
+    })
+    df["EMA9"] = df["close"].ewm(span=9).mean()
+    df["EMA21"] = df["close"].ewm(span=21).mean() 
+    volumen_box.metric(
+        "📊 Volumen",
+         round(df["volume"].iloc[-1], 2)
+    )
 
-            precio_actual = closes[-1]
-            ema9 = df["EMA9"].iloc[-1]
-            ema21 = df["EMA21"].iloc[-1]
-        
-            soporte = df['low'].tail(20).min()
-            resistencia = df['high'].tail(20).max()
-           
-            with st.container():
+    precio_actual = closes[-1]
+    ema9 = df["EMA9"].iloc[-1]
+    ema21 = df["EMA21"].iloc[-1]
 
-               fig = go.Figure(data=[go.Candlestick(
-                   x=df.index,
-                   open=df['open'],
-                   high=df['high'],
-                   low=df['low'],
-                   close=df['close'],
-                   name='BTC/USDT',
+    soporte = df['low'].tail(20).min()
+    resistencia = df['high'].tail(20).max()
+   
+    with st.container():
 
-                   increasing=dict(
-                       line=dict(color='lime'),
-                       fillcolor='lime'
-                   ),
+       fig = go.Figure(data=[go.Candlestick(
+           x=df.index,
+           open=df['open'],
+           high=df['high'],
+           low=df['low'],
+           close=df['close'],
+           name='BTC/USDT',
 
-                   decreasing=dict(
-                       line=dict(color='red'),
-                       fillcolor='red'
-                   )
+           increasing=dict(
+               line=dict(color='lime'),
+               fillcolor='lime'
+           ),
 
-               )])
+           decreasing=dict(
+               line=dict(color='red'),
+               fillcolor='red'
+           )
 
-               fig.add_trace(go.Scatter(
-                   x=df.index,
-                   y=df['EMA9'],
-                   mode='lines',
-                   name='EMA 9'
-               ))
+       )])
 
-               fig.add_trace(go.Scatter(
-                   x=df.index,
-                   y=df['EMA21'],
-                   mode='lines',
-                   name='EMA 21'
-               ))
-               fig.add_hline(
-                   y=soporte,
-                   line_dash="dot",
-                   line_color="green",
-                   annotation_text="Soporte"
-               )
+       fig.add_trace(go.Scatter(
+           x=df.index,
+           y=df['EMA9'],
+           mode='lines',
+           name='EMA 9'
+       ))
 
-               fig.add_hline(
-                   y=resistencia,
-                   line_dash="dot",
-                   line_color="red",
-                   annotation_text="Resistencia"
-               )
-               fig.update_layout(
-                   height=700,
-                   xaxis_rangeslider_visible=False
-               )
+       fig.add_trace(go.Scatter(
+           x=df.index,
+           y=df['EMA21'],
+           mode='lines',
+           name='EMA 21'
+       ))
+       fig.add_hline(
+           y=soporte,
+           line_dash="dot",
+           line_color="green",
+           annotation_text="Soporte"
+       )
 
-               st.plotly_chart(fig, use_container_width=True)
+       fig.add_hline(
+           y=resistencia,
+           line_dash="dot",
+           line_color="red",
+           annotation_text="Resistencia"
+       )
+       fig.update_layout(
+           height=700,
+           xaxis_rangeslider_visible=False
+       )
 
-               with panel.container():
+       st.plotly_chart(fig, use_container_width=True)
 
-                   cambio = precio_actual - closes[-2]
+       with panel.container():
 
-                   st.metric(
-                       "💰 Precio actual",
-                       round(precio_actual, 2),
-                       round(cambio, 2)
-                   )
-                   col1, col2, col3 = st.columns(3)
+           cambio = precio_actual - closes[-2]
 
-                   with col1:
-                       st.write("EMA 9:",round(ema9, 2))
+           st.metric(
+               "💰 Precio actual",
+               round(precio_actual, 2),
+               round(cambio, 2)
+           )
+           col1, col2, col3 = st.columns(3)
 
-                   with col2:
-                       st.write("EMA 21:",round(ema21, 2))
+           with col1:
+               st.write("EMA 9:",round(ema9, 2))
 
-                   with col3:
-                       contador = st.empty()
+           with col2:
+               st.write("EMA 21:",round(ema21, 2))
 
-                       if ema9 > ema21:
-                           st.success("🚀COMPRA SPOT")
+           with col3:
+               contador = st.empty()
 
-                       elif ema9 < ema21:
-                           st.error("📉VENTA SPOT")
+               if ema9 > ema21:
+                   st.success("🚀COMPRA SPOT")
 
-                       st.success(f"Bot iniciado para {crypto}")
-                       for i in range(60, 0, -1):
-                           contador.write(i)
-                           time.sleep(1)
-                       st.rerun()
+               elif ema9 < ema21:
+                   st.error("📉VENTA SPOT")
+
+               st.success(f"Bot iniciado para {crypto}")
+               for i in range(60, 0, -1):
+                   contador.write(i)
+                   time.sleep(1)
+               st.rerun()
