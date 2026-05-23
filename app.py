b22fddb102d5f4541e87aee8378b395b4a74bbc7
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-23 19:08:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import base64
 
 st.set_page_config(
     page_title="CRYPTOSCALPER BOT PRO",
-    page_icon="💀",
+    page_icon="favicon.png",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
