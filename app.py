ac8d9ecf610fee352b148ddb2965baf65dbded06
Cristianbot-tech <cristianmendozaperez07@gmail.com>
2026-05-25 22:15:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -544,9 +544,9 @@ elif pagina == "LIVE":
             )
             chart_placeholder = st.empty()
             
-           with chart_placeholder.container():
-           st.plotly_chart(fig,
-           use_container_width=True)
+            with chart_placeholder.container():
+            st.plotly_chart(fig,
+            use_container_width=True)
 
 
         except Exception as e:
